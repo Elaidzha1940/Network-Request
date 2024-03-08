ca0f3fa59aca1fb99ee004f19eb0fe3b07386425
Elaidzha1940 <eli080293@gmail.com>
2024-03-09 02:25:35 +0300

    Update ViewController

--- Network-Request/ViewController.swift
+++ Network-Request/ViewController.swift
@@ -17,7 +17,15 @@ class ViewController: UIViewController {
     }
     
     private func makeRequest() {
-        var request = URLRequest(url: URL(string: "https://barter.vg/browse/cards/json")!)
+        var request = URLRequest(url: URL(string: "https://openwhyd.org/hot/electro?format=json")!)
+        request.allHTTPHeaderFields = ["authToken": "nil"]
+        request.httpMethod = "Get"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            print(String(decoding: data!, as: UTF8.self))
+            print(error)
+        }
+        task.resume()
     }
 }
 
